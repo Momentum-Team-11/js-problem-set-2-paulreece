db3d28da2d5be103e08d0c070152842e2e334825
Paul Reece <paulreece2@gmail.com>
2022-02-01 22:01:29 -0500

    problems 1-5

--- homework.js
+++ homework.js
@@ -6,17 +6,80 @@
 // If the potential member is not in the array, return the array unchanged.
 // If the potential member is in the array, remove all instances of it from the array.
 
+function remove(students, name) {
+    let newArray = []
+    console.log(students)
+    for (let student of students) {
+    if (student != name) {
+    newArray.push(student)
+} console.log(newArray)
+}
+return newArray
+}
+// function remove(students){
+//     let newarray = []
+//     for (let student of students){
+//       if (students.indexOf(student) > -1){
+//         newarray.push(student)
+//           }
+//         else {
+//             return students
+//         }
+//         console.log(newarray)
+//         return newarray
+//     }
+
+
 // 2. Revisit your "remove" function. Make sure that it does not change the original
 // array but instead returns a new array.
 
 // 3. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
 
+function sum(numbs , single) {
+    let sum = 0
+    for (let single of numbs) {
+        sum = sum + single
+    }
+    return sum
+}
+
 // 4. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
 
+function average(numbers) {
+    let average = 0
+    for (let number of numbers) {
+    return average = sum(numbers) / numbers.length
+    }
+    }
+
 // 5. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
+
+function minimum(numbers) {
+    let minimum = Infinity
+    for (let number of numbers) {
+    return minimum = Math.min(...numbers)
+    }
+    }
+    console.log('minimum: $(min)')
+
+// // function minimum(numbers){
+// // let minimum = Infinity
+// // for (var i = 0; i < numbers.length; i++) {
+// //     if (numbers[i] < minimum) {
+// //         minimum = numbers[i];
+// //     }
+// //     console.log(minimum)
+// // }
+// // }
+
+// function minimum(numbers) {
+//     return Math.min.apply(...numbers)
+    
+// }
+
 
 // 6. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
@@ -41,6 +104,8 @@
 // Note 2: Selection sort can be implemented using one array. Read the explanation at
 // https://courses.cs.vt.edu/csonline/Algorithms/Lessons/SelectionSort/index.html
 // to see how. This may make more sense to you.
+
+
 
 // 7. Create a function called `textList` that takes an array and joins its elements
 // into a string separated by commas.
